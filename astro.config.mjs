d71b971ef34112ae9abb1070be3c88c81dac146f
JuanjoH2 <juanjohuam2@gmail.com>
2025-10-24 12:28:47 -0600

    RSS for the published newsletters using Kit's API implemented correctly (need to update to retrieve the element itself, and not redirect the user to Kit's website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        // This makes sure the public environment variables are correctly loaded
+        // and accessible via import.meta.env.PUBLIC_CONVERTKIT_API_KEY
+        envPrefix: 'PUBLIC_',
+    }
+});
